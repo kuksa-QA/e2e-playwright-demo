8c0b26ec471eb9574a1c4df2b41398373e72e448
Ekaterina Kuksa <mishelledale@gmail.com>
2026-08-25 09:47:56 +0300

    test: wait for scroll-up plugin, add ad-blocking diagnostic

--- tests/ui/home-page.spec.ts
+++ tests/ui/home-page.spec.ts
@@ -1,40 +1,44 @@
-import {expect, test} from '@playwright/test';
-
-import { HomePage } from '../../pages/home-page';
+import { expect, test } from '../../fixtures/pages';
 
 test.describe.configure({ mode: 'parallel' });
 
 test.describe('Home page', () => {
-    let homePage: HomePage;
-    test.beforeEach(async ({page}) => {
-        homePage = new HomePage(page)
-        await homePage.navigate()
+    test.beforeEach(async ({ homePage }) => {
+        await homePage.navigate();
     })
 
-    test('Header UI', async () => {
+    test('Header UI', async ({ homePage }) => {
         await expect(homePage.header.homeButton).toHaveCSS('color', 'rgb(255, 165, 0)')
     })
 
-    test('Carousel change', async () => {
-        await expect.soft(homePage.carouselIndicator(0)).toHaveCSS('background-color', 'rgb(254, 152, 15)');
+    test('Carousel change', async ({ homePage }) => {
+        await homePage.pauseCarousel();
+
+        await expect.soft(homePage.carouselIndicator(0)).toHaveClass(/active/);
         await homePage.carouselNextButton.click();
-        await expect.soft(homePage.carouselIndicator(1)).toHaveCSS('background-color', 'rgb(254, 152, 15)');
+        await expect.soft(homePage.carouselIndicator(1)).toHaveClass(/active/);
         await homePage.carouselNextButton.click();
-        await expect.soft(homePage.carouselIndicator(2)).toHaveCSS('background-color', 'rgb(254, 152, 15)');
+        await expect.soft(homePage.carouselIndicator(2)).toHaveClass(/active/);
         await homePage.carouselPrevButton.click();
-        await expect(homePage.carouselIndicator(1)).toHaveCSS('background-color', 'rgb(254, 152, 15)');
+        await expect(homePage.carouselIndicator(1)).toHaveClass(/active/);
     })
 
-    test('Scroll Up button appears after scroll', async () => {
-        await homePage.page.mouse.wheel(0, 2000)
-        await expect(homePage.scrollUpButton).toBeVisible()
+    test('Scroll Up button appears after scroll', async ({ homePage }) => {
+        await expect(homePage.scrollUpButton).toBeAttached();
+ 
+        await homePage.page.mouse.wheel(0, 2000);
+        await expect(homePage.scrollUpButton).toBeVisible();
     })
 
-    test('Scroll Up button scrolls up', async () => {
-        const START_POSITION = await homePage.page.locator('header').boundingBox()
-        await homePage.page.mouse.wheel(0, 2000)
-        await homePage.scrollUpButton.click({force: true})
-        await expect(homePage.scrollUpButton).toBeHidden()
-        expect(await homePage.page.locator('header').boundingBox()).toEqual(START_POSITION)
+    test('Scroll Up button scrolls up', async ({ homePage }) => {
+        await expect(homePage.scrollUpButton).toBeAttached();
+ 
+        await homePage.page.mouse.wheel(0, 2000);
+        await expect(homePage.scrollUpButton).toBeVisible();
+ 
+        await homePage.scrollUpButton.click();
+ 
+        await expect(homePage.scrollUpButton).toBeHidden();
+        await expect.poll(() => homePage.page.evaluate(() => window.scrollY)).toBe(0);
     })
 })
